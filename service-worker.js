6553d2344ebb224bf6afd45052a7307c9d9471d0
antkong <kimhee0010@naver.com>
2024-09-30 17:43:57 +0900

    test

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('smu-d-cache').then(function(cache) {
+      caches.open('smu-restaurant-cache').then(function(cache) {
         return cache.addAll([
           '/',
           '/index.html',
           '/css/styles.css',
-          '/js/main.js',
+          '/js/app.js',
           '/images/icons/icon-192x192.png',
           '/images/icons/icon-512x512.png'
         ]);
